72b9602c7680a8b07bcf142598552b40eacfeb87
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-10-10 15:12:00 +0300

    Feat: create a file and folder structure

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function AppHeader() {
+  return (
+    <>
+    <BurgerIcon />
+      <Logo />
+    </>
+  );
+}
+
+export default AppHeader;
---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AppHeader from "../app-header/app-header"
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+   
+    </div>
+  );
+}
+
+export default App;
