d1f85e454ec96f392ccc732ec6876ed4e9c51556
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-12-25 22:45:38 +0300

    Feat: add Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./Preloader.css";
+import React from 'react';
+import logoSVG from '../../images/svg/logo.svg';
+import './Preloader.css';
 
 const Preloader = () => {
   return (
-    <div className="preloader">
-      <div className="preloader__container">
-        <span className="preloader__round"></span>
+    <div className='preloader'>
+      <div className='preloader__container'>
+        <img  className="preloader__round" src={logoSVG} alt='Загрузка...' />
       </div>
     </div>
   );
